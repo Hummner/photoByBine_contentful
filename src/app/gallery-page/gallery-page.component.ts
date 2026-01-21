c42e1ec7d38cb37ad102ed530f388bc12070f03b
Hummner <bence.cservenyak@gmail.com>
2026-01-21 13:50:52 +0100

    add new animation and fix contentful loadData()

--- src/app/gallery-page/gallery-page.component.ts
+++ src/app/gallery-page/gallery-page.component.ts
@@ -17,25 +17,35 @@ export class GalleryPageComponent implements OnInit {
   ngOnInit(): void {
     this.contentfulService.getGallery().subscribe(
       (res) => {
-        this.categories = this.loadData(res.items);
+        this.categories = this.loadData(res);
         this.photos = res.includes.Assets
         console.log(this.categories);
-        console.log(this.photos);
+        console.log(res);
 
       }
     )
   }
 
-  loadData(items: any) {
+  loadData(res: any) {
+    const items = res.items
+    const photos = res.includes.Asset
     return this.categories = items.map((category: any) => {
       return {
         title: category.fields.title,
         description: category.fields.description,
         photoIds: category.fields.photos,
-        coverImage: category.fields.coverImage
+        coverImage: this.loadCoverImage(photos, category.fields.coverImage.sys.id)
       }
     }
-
     )
+  }
+
+  findAssetById(assets: any, id: string) {
+    return assets.find((asset: any) => asset.sys.id === id)
+  }
+
+  loadCoverImage(photos: any, fieldId: string) {
+    const image = this.findAssetById(photos, fieldId)
+    return image.fields.file.url
   }
 }
