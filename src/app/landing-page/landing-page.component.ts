227251cf7fd35e3deb4e4508c878f008bc5346d1
Hummner <bence.cservenyak@gmail.com>
2026-01-28 13:28:23 +0100

    add: news page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HEROComponent } from "./hero/hero.component";
+import { NewsComponent } from "./news/news.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [HEROComponent],
+  imports: [HEROComponent, NewsComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
